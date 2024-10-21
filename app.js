12de6c240ddb246880aa7c105b65e949762eecc0
Atomic207 <elijah.rietmann@icloud.com>
2024-10-21 11:43:12 +0200

    joki stuff

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-Vue.createApp({
-  data() {
-    return {};
-  },
-  methods: {},
-}).mount("#app");
+const goalElement = document.getElementById("goal");
+const buttonElement = document.getElementById("submit");
+const listElement = document.getElementById("list");
+
+function addGoal() {
+  const goal = goalElement.value.trim();
+  if (goal === "") {
+    alert("Enter a goal >:(");
+    return;
+  }
+  const liElement = document.createElement("li");
+  liElement.textContent = goal;
+  listElement.appendChild(liElement);
+}
+//Element aus der Liste entfernen
+//function removeGoal(event) {
+// const liElement = event.target;
+//listElement.removeChild(liElement);
+//}
+function markGoalDone(event) {
+  const liElement = event.target;
+  liElement.classList.toggle("done");
+}
+listElement.addEventListener("click", markGoalDone);
+// listElement.addEventListener("click", removeGoal);
+buttonElement.addEventListener("click", addGoal);
